664e6865e1de61ec40435841f24235ba6ac70fdb
eliot martin <eliot.martin@epitech.eu>
2022-09-26 23:56:28 +0200

    fix scroll figure

--- src/Page.js
+++ src/Page.js
@@ -4,43 +4,40 @@ import './Page.css'
 
 function Page() {
       const top = useRef();
-      const info = useRef();
       const experience = useRef();
       const parcours = useRef();
       const loisir = useRef();
     return (
-        <div>
-            <Header top={top} loisir={loisir} info={info} experience={experience} parcours={parcours}/>
+        <body src='' alt>
+            <Header top={top} loisir={loisir} experience={experience} parcours={parcours}/>
             <div className='Content'>
-                <article>
                     <figure ref={top}>
                         <div>
-                            <h1>1</h1>
+                            <h1>Talk about me</h1>
                         </div>
                     </figure>
-                    <section ref={info}>
+                    <section>
                         <div>
-                            <h1>2</h1>
+                            <h1>talk about experiences</h1>
                         </div>
                     </section>
                     <figure ref={experience}>
                         <div>
-                            <h1>3</h1>
+                            <h1>talk about experiences</h1>
                         </div>
-                        </figure>
+                    </figure>
                     <section ref={parcours}>
                         <div>
-                            <h1>4</h1>
+                            <h1>talk about parcours</h1>
                         </div>
                     </section>
                     <figure ref={loisir}>
                         <div>
-                            <h1>5</h1>
+                            <h1>talk about loisir</h1>
                         </div>
                     </figure>
-                </article>
             </div>
-        </div>
+        </body>
 
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,15 @@
 import './Header.css'
 
-function Header({top, info, experience, parcours, loisir}) {
+function Header({top, experience, parcours, loisir}) {
   return (
     <header className="Header">
       <div onClick={() => top.current.scrollIntoView()} >
         <h1 >ELIOT MARTIN</h1>
       </div>
       <div className="Header Container">
-        <div className="Header Index" onClick={() => info.current.scrollIntoView()}>Info sur moi</div>
-        <div className="Header Index" onClick={() => experience.current.scrollIntoView()}>Experience Pro</div>
-        <div className="Header Index" onClick={() => parcours.current.scrollIntoView()}>Parcours</div>
-        <div className="Header Index" onClick={() => loisir.current.scrollIntoView()}>Loisir</div>
+        <h1 className="Index" onClick={() => experience.current.scrollIntoView()}>Experience Pro</h1>
+        <h1 className="Index" onClick={() => parcours.current.scrollIntoView()}>Parcours</h1>
+        <h1 className="Index" onClick={() => loisir.current.scrollIntoView()}>Loisir</h1>
       </div>
     </header>
     
