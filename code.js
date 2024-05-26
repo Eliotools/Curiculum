26ec7160bddf11d25575012dbed7e501bbdcbfa4
eliot martin <eliot@MacBook-Pro-de-eliot.local>
2024-05-26 14:47:26 +0200

    add interim exp

--- code.js
+++ code.js
@@ -336,6 +336,14 @@ experience : {
                 "desc" : ["Maintient d’une application de gestion de stock", "<b>HTML / CSS & Google AppScript"],
                 "image" : "https://cdn6.aptoide.com/imgs/c/5/d/c5d8abab5e239065201a510f9fc15141_icon.png"
             },{
+                "dateStart" : "06/21",
+                "dateEnd" : "08/21",
+                "name" : "Castorama",
+                "lenght" :"2 mois",
+                "type" : "Interim",
+                "desc" : ["Job d'été", "Mise en rayon", "Préparation de commande drive"],
+                "image" : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAh1BMVEX////2+v0IetcIfNgRgNkAdtYAeNfp8/ujzO9ztOhkrOZ9ueqw1PLi7/pbp+W32PMojNwXg9o/mODH4PX6/P4jidxQoePY6vjN5Pb/+uD/7X//63OKrWGBqmsihLz/4B//3ADl0RgJe9CVxe0vkN4ohrj/5UPBwzX/9sH/9LdLlJpBkKJtsegsk00aAAAAyklEQVR4AdSSQxYEMRRFg7JtG/tfX6OsnB73m0Q334AgiPAkigjQzCT8bwDL8YIgSpAAyAparnj4BKgUs0mT74CKmIOUG8Dq83HGNPYG8NPBMC0o2Ywt32KwqOndmow57j0Lc9p7W0Z+EIZRFCcbwH3fdbApzSblxQqUX6DaDWSLapKFZn5v0TkGcye6vh+GEe9Z6JMPOO0FCQBCHSqJZaWKQRIgVnK5lG4AMPGRcH5004EPAGCFxYjNkSYKSuJ7OpzKwkMxXxDM/gCBTxHm74MunQAAAABJRU5ErkJggg=="
+            },{
                 "dateStart" : "09/20",
                 "dateEnd" : "12/20",
                 "name" : "Sigmatel",
@@ -343,8 +351,7 @@ experience : {
                 "type" : "Stage",
                 "desc" : ["Creation d'une CVteck", "Application windows" ,"<b>Python"],
                 "image" : "https://www.sygmatel.fr/wp-content/uploads/2021/04/Sygmatel-LOW.jpg"
-            },
-            {
+            },{
                 "dateEnd" : "01/19",
                 "dateStart" : "<br>",
                 "name" : "Respir@ction",
@@ -352,6 +359,23 @@ experience : {
                 "type" : "Hackathon",
                 "desc" : ["Ideation d’un projet autour de l’allergie", "<b>Marketing, User Experience, Maquette", "Travail en collaboration avec l'Afpral"],
                 "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpESjnY33osAlDQo1tLFSAY6ccmYf014IHLTBm9AkDWA&s"
+            },{
+                "dateStart" : "07/18",
+                "dateEnd" : "08/18",
+                "name" : "Pheonix Pharma",
+                "lenght" :"1 mois",
+                "type" : "Interim",
+                "desc" : ["Job d'été", "Agent de quai", "Stockage de produits pharmaceutique", "Création de commande clients"],
+                "image" : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABDElEQVR4AYzQgQbCQByA8ZKBEMKCSUgIhBACbElICIEEQggECIQQyEaPEAIBkkySSULPEHqM9V1uuXQy/Nz8d75zlwjDMCnkPCeFPCam52xZb/AR4IQF6jDE/ogasHGAi0bOs4usJixUMZfREdKfgDy5iatcTR15iIUzVjCiQBFH2DLma7nvK5RQwQWtKLDETLlK+MeDq2XlWwS8VUEEeuJDEwg4tcs65v9dmXdgYAjr84g/AdfZKLN+NCc2VfdrA9IOZdSwVuaDeAG9JzJfAQYFGDECe7SVfSYyIuBzr9eM8YUBFleyQg10h0cjaQYgohEjIREyAC0hYSZlUNxD498SQzNmUqY8M1GcnQGOkvCOpiJKjwAAAABJRU5ErkJggg=="
+            },
+            {
+                "dateStart" : "07/17",
+                "dateEnd" : "08/17",
+                "name" : "Sigmatel",
+                "lenght" :"1 mois",
+                "type" : "Interim",
+                "desc" : ["Job d'été", "Électricien", "Instalisation éélctrique dans un immeuble, "],
+                "image" : "https://www.sygmatel.fr/wp-content/uploads/2021/04/Sygmatel-LOW.jpg"
             }
         ]
     },
