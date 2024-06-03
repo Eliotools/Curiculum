f7e9a1cc71330e80a203374c06006790a557462f
Eliotools <75935267+Eliotools@users.noreply.github.com>
2024-06-03 19:57:32 +0200

    set ariable trad

--- main.js
+++ main.js
@@ -169,7 +169,7 @@ switchContent = (type, node) => {
 
 
 
-data = {
+data_fr = {
     formation : [    {
         "name" : "Epitech Lille",
         "desc" : ["Master d'expert en technologies de l'information.", "Approfondissement gestion de projet"],
@@ -327,8 +327,168 @@ data = {
         }
 }
 
+data_en = {
+    formation : [    {
+        "name" : "Epitech Lille",
+        "desc" : ["Master's degree in information technology.", "Project management"],
+        "date" : '2024',
+        "image" : "https://yt3.googleusercontent.com/8mA9eT4kl-CHHHQdCtNfMCWeL6RFFQElfY1ytMoNHF5BG1iMtHQ0b9YaonhC7bydvrG-9hkA=s900-c-k-c0x00ffffff-no-rj"
+    },{
+        "name" : "Technological University Dublin",
+        "desc" : ["IT management diploma", "Strategic & project Management (Scrum / Agile)", "Business Modeling"],
+        "date" : '2023',
+        "image" : "https://www.ieabroad.com/wp-content/uploads/TUD.png"
+    }, {
+        "name" : "Epitech Lille",
+        "desc" : ["Bachelor's degree", "Project-based teaching", "Group work"],
+        "date" : '2022',
+        "image" : "https://yt3.googleusercontent.com/8mA9eT4kl-CHHHQdCtNfMCWeL6RFFQElfY1ytMoNHF5BG1iMtHQ0b9YaonhC7bydvrG-9hkA=s900-c-k-c0x00ffffff-no-rj"
+    }, {
+        "name" : "Lycée Guist'hau, Nantes",
+        "desc" : ["Bacalaurea Scientific","Mathematics"],
+        "date" : '2019',
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXFOGuHabIn7GjpXicZ1p0cwAHPspmnwt7PYyUV4usbg&s"
+    }],
+    experience : [
+            {
+                "dateStart" : "06/23",
+                "dateEnd" : "Now",
+                "name" : "Jagger-Lewis",
+                "lenght" :"1 year",
+                "type" : "CDI",
+                "desc" : ["Mobile application in <b>Flutter</b>.", "Development of an API <b>JS<b>.", "Pipeline development in <b>Python<b>.", "Implementation of <b>CI</b> tools on various solutions"],
+                "image" : 'https://assets-global.website-files.com/6549f4ba8294cf140608d893/654b6a20bf057d68593d620a_logo.png'
+            }, {
+                "dateStart" : "03/22",
+                "dateEnd" : "06/22",
+                "name" : "Chantier Carré",
+                "lenght" :"4 months",
+                "type" : "Internship",
+                "desc" : ["Web application in <b>Next JS<b>.", "Server Less", "Management of a team of 3 people", "Agile<b> organization"],
+                "image" : "https://media.licdn.com/dms/image/C4D0BAQF7XnLK8hBAcQ/company-logo_200_200/0/1630474703807/chantiercarr_logo?e=1723075200&v=beta&t=BFU4ASoiBSeIvH_fwp8DgC7LO9ov8Nr9iXf84lGfnbY"
+            },{
+                "dateStart" : "09/21",
+                "dateEnd" : "02/22",
+                "name" : "Décathlon Btwin Village",
+                "lenght" :"8 months",
+                "type" : "Stage 2 jours semaine",
+                "desc" : ["Maintenance of a stock management application", "<b>HTML / CSS & Google AppScript"],
+                "image" : "https://cdn6.aptoide.com/imgs/c/5/d/c5d8abab5e239065201a510f9fc15141_icon.png"
+            },{
+                "dateStart" : "06/21",
+                "dateEnd" : "08/21",
+                "name" : "Castorama",
+                "lenght" :"2 months",
+                "type" : "Interim",
+                "desc" : ["Summer job", "Shelving", "Drive order picking"],
+                "image" : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAh1BMVEX////2+v0IetcIfNgRgNkAdtYAeNfp8/ujzO9ztOhkrOZ9ueqw1PLi7/pbp+W32PMojNwXg9o/mODH4PX6/P4jidxQoePY6vjN5Pb/+uD/7X//63OKrWGBqmsihLz/4B//3ADl0RgJe9CVxe0vkN4ohrj/5UPBwzX/9sH/9LdLlJpBkKJtsegsk00aAAAAyklEQVR4AdSSQxYEMRRFg7JtG/tfX6OsnB73m0Q334AgiPAkigjQzCT8bwDL8YIgSpAAyAparnj4BKgUs0mT74CKmIOUG8Dq83HGNPYG8NPBMC0o2Ywt32KwqOndmow57j0Lc9p7W0Z+EIZRFCcbwH3fdbApzSblxQqUX6DaDWSLapKFZn5v0TkGcye6vh+GEe9Z6JMPOO0FCQBCHSqJZaWKQRIgVnK5lG4AMPGRcH5004EPAGCFxYjNkSYKSuJ7OpzKwkMxXxDM/gCBTxHm74MunQAAAABJRU5ErkJggg=="
+            },{
+                "dateStart" : "09/20",
+                "dateEnd" : "12/20",
+                "name" : "Sygmatel",
+                "lenght" :"4 months",
+                "type" : "Internship",
+                "desc" : ["Creating a CVteck", "Application windows" ,"<b>Python"],
+                "image" : "https://www.sygmatel.fr/wp-content/uploads/2021/04/Sygmatel-LOW.jpg"
+            },{
+                "dateEnd" : "01/19",
+                "dateStart" : "<br>",
+                "name" : "Respir@ction",
+                "lenght" :"2 days",
+                "type" : "Hackathon",
+                "desc" : ["Ideation of an allergy project", "<b>Marketing, User Experience, Model", "Working in collaboration with Afpral"],
+                "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpESjnY33osAlDQo1tLFSAY6ccmYf014IHLTBm9AkDWA&s"
+            },{
+                "dateStart" : "07/18",
+                "dateEnd" : "08/18",
+                "name" : "Pheonix Pharma",
+                "lenght" :"1 month",
+                "type" : "Interim",
+                "desc" : ["Summer job", "Dock agent", "Storage of pharmaceutical products", "Customer order creation"],
+                "image" : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABDElEQVR4AYzQgQbCQByA8ZKBEMKCSUgIhBACbElICIEEQggECIQQyEaPEAIBkkySSULPEHqM9V1uuXQy/Nz8d75zlwjDMCnkPCeFPCam52xZb/AR4IQF6jDE/ogasHGAi0bOs4usJixUMZfREdKfgDy5iatcTR15iIUzVjCiQBFH2DLma7nvK5RQwQWtKLDETLlK+MeDq2XlWwS8VUEEeuJDEwg4tcs65v9dmXdgYAjr84g/AdfZKLN+NCc2VfdrA9IOZdSwVuaDeAG9JzJfAQYFGDECe7SVfSYyIuBzr9eM8YUBFleyQg10h0cjaQYgohEjIREyAC0hYSZlUNxD498SQzNmUqY8M1GcnQGOkvCOpiJKjwAAAABJRU5ErkJggg=="
+            },
+            {
+                "dateStart" : "07/17",
+                "dateEnd" : "08/17",
+                "name" : "Sygmatel",
+                "lenght" :"1 month",
+                "type" : "Interim",
+                "desc" : ["Summer job", "Electrician", "Electrical installation in a building"],
+                "image" : "https://www.sygmatel.fr/wp-content/uploads/2021/04/Sygmatel-LOW.jpg"
+            }
+    ],
+    competence : 
+         [
+            {
+                name : 'HTML/CSS',
+                value : '4',
+                color : 'rgba(47, 106, 241, 0.2)',
+                graph : {
+                    values : ['web', 'animation', 'responive', 'autonomie'],
+                    keys : ['100', '20', '80', '100'] 
+                },
+            }, {
+                name : 'JavaScript',
+                value : '5',
+                color : 'rgba(247, 224, 41, 0.5)',
+                graph : {
+                    values : ['web', 'animation', 'responive', 'autonomie'],
+                    keys : ['100', '20', '80', '100'] 
+                },
+            }, {
+                name : 'React Native',
+                value : '4',
+                color : 'rgba(103, 218, 251, 0.5)',
+                graph : {
+                    values : ['web', 'animation', 'responive', 'autonomie'],
+                    keys : ['100', '20', '80', '100'] 
+                },
+            },  {
+                name : 'Flutter',
+                value : '5',
+                color : 'rgba(89, 199, 248, 0.5)',
+                graph : {
+                    values : ['web', 'animation', 'responive', 'autonomie'],
+                    keys : ['100', '20', '80', '100'] 
+                },
+            },  {
+                name : 'Python',
+                value : '5',
+                color: 'rgba(89, 199, 248, 0.5)',
+                graph : {
+                    values : ['web', 'animation', 'responive', 'autonomie'],
+                    keys : ['100', '20', '80', '100'] 
+                },
+            },  {
+                name : 'C',
+                value : '4',
+                color : 'rgba(62, 117, 164, 0.5)',
+                graph : {
+                    values : ['web', 'animation', 'responive', 'autonomie'],
+                    keys : ['100', '20', '80', '100'] 
+                },
+            },  {
+                name : 'C++',
+                value : '3',
+                color : 'rgba(105, 157, 211, 0.5)',
+                graph : {
+                    values : ['web', 'animation', 'responive', 'autonomie'],
+                    keys : ['100', '20', '80', '100'] 
+                },
+            },
+        ],
+        eip : {
+            description : "The Epitech Inovative Project is the end-of-study project at Epitech<br>From 3rd to 5th year, groups of 4 to 10 students create their project and push it to a hypothetical commercialization.<br><br><br>.",
+            dedal_description : "For 2 years, I was in charge of the DEDAL group, with whom we made it all the way to the national finals to present our project for a mobile application to simplify tourist visits.",
+            image : 'asset/dedal.png',
+            showcase : 'https://dedal-showcasewebsite.vercel.app/'
+        }
+}
+
 let ratio = window.screen.availWidth < 600
 
+
+let local = navigator.language.includes('fr') ? data_fr : data_en
 document.querySelectorAll("[id^=action]").forEach((node) => {
     if (redirect[node.id]) {
         redirect[node.id](node)
